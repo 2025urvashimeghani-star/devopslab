c1db0184a4430e297f10e902840d9beb1695f18c
sumitb252 <2025.sumit.bhatia@ves.ac.in>
2026-02-11 15:14:34 +0530

    Added AddNumbers with exec plugin

--- springbootapp/src/main/java/com/example/springbootapp/AddNumbers.java
+++ springbootapp/src/main/java/com/example/springbootapp/AddNumbers.java
@@ -1,11 +1,12 @@
+package com.example.springbootapp;
+
 public class AddNumbers {
+
     public static void main(String[] args) {
 
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
 
-        int sum = a + b;
-
-        System.out.println("Sum = " + sum);
+        System.out.println("Sum = " + (a + b));
     }
 }
